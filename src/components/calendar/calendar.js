00789768352c2fc32084432a8e19a10b34b24189
Sergio Posse <sergiodavidposse@gmail.com>
2020-06-14 09:29:46 -0300

    first commit moment installed calendar component almost done

---
+++ src/components/calendar/calendar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import moment from 'moment';
+
+export default class Calendar extends React.Component{
+    render(){
+        return(
+            <div className='calendar-container'>
+                <h2>CALENDAR</h2>
+            </div>
+        );
+        
+    }
+
+}
